b64a340870f526233aca06b0074df94dc767968b
Roope <roope@blowback.fi>
2020-02-24 14:44:20 +0200

    1.2.0 update

--- README.md
+++ README.md
@@ -1,27 +1,66 @@
-# ProcessWire Email Obfuscation
+# ProcessWire Email Obfuscation (EMO)
 
-**Email Obfuscation (EMO) module for plaintext emails and email links with 64 base crypting**
+**Email Obfuscation module for email addresses with 64 base crypting**
 
-This module finds all plaintext emails and email links from the content and replaces them with span elements. All the 
-addresses are stored in 64 base encoded srings to JavaScript array in souce code. Then on client side browser finds 
-emo_email span elements, replaces and encodes them back to email addresses.
-
-All emails inside form elements are left untouched. Same as anchor texts, unless it's email address. You can define 
-default text string for replaced email address in module config.
-
-Both emo touched span and anchor elements have `emo_email` class name that you can use for css/js.
+This module finds all plaintext emails and email links from the document and
+replaces them with span elements including configurable replace text. All the
+addresses are encoded to 64 base strings and stored in spans data attributes.
+Then on client side we decode these strings back to their original state.
 
 ## Install
 
-1. Create a new folder `EmailObfuscation` into `/site/modules/`.
-2. Copy the contents of this repository to `/site/modules/EmailObfuscation/`.
-3. Login to processwire and go to Modules page and click 'Check for new modules'. Find emo and click install.
-4. You can make optional configuration changes in module admin page.
+Install this module using standard [install procedure in ProcessWire][1].
+
+## Options
+
+After install you can find some configurable options on module admin page.
+
+### Replace text string
+
+Transliterable text string used as a replace to obfuscated email address.
+
+### Obfuscation mode
+
+There is three modes available for this module to handle obfuscation.
+
+1. Obfuscate manually by using `$sanitizer->emo($str)` method.
+2. Obfuscate automatically at selected templates/pages.
+3. Obfuscate automatically but exclude selected templates/pages (default).
+
+### JavaScript loading method
+
+1. Load file manually.
+2. Load file to `$config->scripts` array.
+3. Append automatically to page as external script (default).
+4. Append automatically to page as inline script.
+
+### Force mailto
+
+By enabling force mailto option all email addresses are rendered as mailto
+links regardless of their original state.
+
+### Fixed encrypt key
+
+When enabled encryption key is locked and does not change. By default encryption
+key updates on every session. Fixed key is required when you cache obfuscated
+AJAX output for more than session lifetime.
+
+### Debug
+
+Appends debug data to HTML and console output.
+
+### Selected Templates / Pages
+
+List of selected templates/pages that are used to include or exclude at
+automatic obfuscation.
 
 ## Thanks
 
-This is ProcessWire module fork from [MODX Evolution plugin emo E-Mail Obfuscation](http://modx.com/extras/package/emoemailobfuscation).
+This is ProcessWire module fork from [MODX Evolution plugin][2].
 
 ## License
 
-[GNU Public License (GPL)](http://www.gnu.org/copyleft/gpl.html)
+[MIT License](http://opensource.org/licenses/MIT)
+
+[1]: https://modules.processwire.com/install-uninstall/
+[2]: http://modx.com/extras/package/emoemailobfuscation
--- emo.js
+++ emo.js
@@ -1,69 +1,72 @@
 /*
- *  Written by: Florian Wobbe
- *  Contact: www.eprofs.de
- *  Created: 2008-01-12 - v1.1
- *  Modified: 2008-01-17 - v1.2 - Email encoding
- *  Modified: 2008-01-19 - v1.3 - Custom Base64 encoding
- *  Modified: 2013-02-27 - v1.4 - Unicode characters fix
- *  Modified: 2018-06-10 - v1.5 - Remove onload event init
- *  Modified: 2018-12-31 - v1.6 - Convert to object literal + auto init onload
- *  Name: EMO Email Obfuscation
- *  Description: These javascript functions replace html placeholder
- *    span elements with decrypted email addresses.
- *  License: GPL
+ * Email Obfuscation (EMO)
+ *
+ * Email Obfuscation module for email addresses with 64 base crypting.
+ *
+ * https://github.com/BlowbackDesign/EmailObfuscation
+ * Copyright (C) 2020 Blowback https://www.blowback.fi
+ * MIT license http://opensource.org/licenses/MIT
+ *
+ * This ProcessWire module is originally a fork from a MODX Evolution plugin:
+ * https://modx.com/extras/package/emoemailobfuscation
+ *
  */
 
+var emo = emo || {};
 
-var emo = {
-
-  init: function() {
-    if(typeof(emo_addr) === 'undefined') return;
-    emo.replace();
-  },
-
-  replace: function() {
-    for(var i = 1; i < emo_addr.length; i++) {
-      var id = 'emo_email_' + i;
-      var elem = document.getElementById(id);
-      if(elem) {
-        if(elem.firstChild) {
-          elem.removeChild(elem.firstChild);
-        }
-        elem.outerHTML = emo.decrypt(i);
-      }
-    }
-  },
-
-  decrypt: function(n) {
-    if(emo.cache[n]) return emo.cache[n];
-    if(emo_addr[n]) var crypted = emo_addr[n];
-    if(!crypted.length) return "Emo decrypt error, not a valid index.";
-    emo.cache[n] = decodeURIComponent(escape(emo.decode(crypted)));
-    return emo.cache[n];
-  },
-
-  decode: function(data) {
-    var tab = emo_addr[0];
-    var out = "", c1, c2, c3, e1, e2, e3, e4;
-    for(var i = 0; i < data.length;) {
-      e1 = tab.indexOf(data.charAt(i++));
-      e2 = tab.indexOf(data.charAt(i++));
-      e3 = tab.indexOf(data.charAt(i++));
-      e4 = tab.indexOf(data.charAt(i++));
-      c1 = (e1 << 2) + (e2 >> 4);
-      c2 = ((e2 & 15) << 4) + (e3 >> 2);
-      c3 = ((e3 & 3) << 6) + e4;
-      out += String.fromCharCode(c1);
-      if(e3 != 64) out += String.fromCharCode(c2);
-      if(e4 != 64) out += String.fromCharCode(c3);
-    }
-    return out;
-  },
-
-  cache: new Array(),
-
+emo.init = function() {
+  if(emo.debug === true) {
+    console.log('EMO inited!\nUsing key: ' + emo.key);
+  }
+  emo.replace(document);
 };
 
-window.addEventListener('load', function() {
+emo.cache = new Array();
+
+emo.replace = function(element) {
+  var el = element.querySelectorAll('[data-emo]');
+  var decrypted = '';
+  for(var i = 0; i < el.length; i++) {
+    if(el[i]) {
+      if(emo.debug === true) console.log(i + ': ' + el[i].outerHTML);
+      if(el[i].firstChild) {
+        el[i].removeChild(el[i].firstChild);
+      }
+      decrypted = emo.decrypt(el[i].getAttribute('data-emo'));
+      if(emo.debug === true) console.log(i + ': ' + decrypted);
+      el[i].outerHTML = decrypted;
+    }
+  }
+};
+
+emo.decrypt = function(id) {
+  if(!emo.cache[id]) {
+    emo.cache[id] = decodeURIComponent(escape(emo.decode(id)));
+  }
+  return emo.cache[id];
+};
+
+emo.decode = function(data) {
+  var out = "", c1, c2, c3, e1, e2, e3, e4;
+  for(var i = 0; i < data.length;) {
+    e1 = emo.key.indexOf(data.charAt(i++));
+    e2 = emo.key.indexOf(data.charAt(i++));
+    e3 = emo.key.indexOf(data.charAt(i++));
+    e4 = emo.key.indexOf(data.charAt(i++));
+    c1 = (e1 << 2) + (e2 >> 4);
+    c2 = ((e2 & 15) << 4) + (e3 >> 2);
+    c3 = ((e3 & 3) << 6) + e4;
+    out += String.fromCharCode(c1);
+    if(e3 != 64) out += String.fromCharCode(c2);
+    if(e4 != 64) out += String.fromCharCode(c3);
+  }
+  return out;
+};
+
+if(document.readyState === 'complete') {
   emo.init();
-});
+} else {
+  window.addEventListener('load', function() {
+    emo.init();
+  });
+}
